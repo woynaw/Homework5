2de626fe3be59f99f3ef7bfef7d3c4fddaf4aa17
Войналович Владислав <woynaw@hotmail.com>
2023-05-29 09:25:49 +0300

    add Zadacha34

--- Zadacha34/Program.cs
+++ Zadacha34/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] FillArray()
+{
+    int[] array = new int[5];
+    int index = 0;
+    while (index < array.Length)
+    {
+        array[index] = new Random().Next(100, 1000);
+        index++;
+    }
+    return array;
+}
+
+int EvenCount(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0) count++;
+    }
+    return count;
+} 
+
+void PrintArray(int[] array)
+{
+   
+    Console.Write("[");
+
+    for (int i = 0; i < array.Length; i++)
+    {   
+        if (i == array.Length - 1) System.Console.Write($"{array[i]}");
+        else System.Console.Write($"{array[i]},");
+    }
+    Console.Write("]");
+}
+int[] array = FillArray();
+PrintArray(array);
+Console.WriteLine($" -> {EvenCount(array)}");
