85b790113de02a86c4a1e473bef187b6f843a47d
Войналович Владислав <woynaw@hotmail.com>
2023-05-29 09:30:56 +0300

    add Zadacha36

--- Zadacha36/Program.cs
+++ Zadacha36/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] FillArray()
+{
+    int[] array = new int[4];
+    int index = 0;
+    while (index < array.Length)
+    {
+        array[index] = new Random().Next(-100, 100);
+        index++;
+    }
+    return array;
+}
+
+int OddSum(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i % 2 != 0) sum += array[i];
+    }
+    return sum;
+} 
+
+void PrintArray(int[] array)
+{
+   
+    Console.Write("[");
+
+    for (int i = 0; i < array.Length; i++)
+    {   
+        if (i == array.Length - 1) System.Console.Write($"{array[i]}");
+        else System.Console.Write($"{array[i]},");
+    }
+    Console.Write("]");
+}
+
+int[] array = FillArray();
+PrintArray(array);
+Console.WriteLine($" -> {OddSum(array)}");
